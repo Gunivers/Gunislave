6d48a3f84db98f16aed236354f1e4151603a682d
theogiraudet <theo.giraudet@orange.fr>
2020-06-11 17:03:34 +0200

    Complete Test Command

--- src/main/java/net/gunivers/gunislave/command/commands/TestCommand.java
+++ src/main/java/net/gunivers/gunislave/command/commands/TestCommand.java
@@ -1,7 +1,9 @@
 package net.gunivers.gunislave.command.commands;
 
 import fr.theogiraudet.json_command_parser.Command;
+import fr.theogiraudet.json_command_parser.Ignore;
 
+@Ignore
 public class TestCommand extends Command {
 
     @Override
