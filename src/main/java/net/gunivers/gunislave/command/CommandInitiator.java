6a5a9787dcf4bc77bb672593a2f2c46022b5b9e7
AZ <35401882+AZ-0@users.noreply.github.com>
2020-06-12 09:42:54 +0200

    Retrieve ExecutionResult upon command execution
    The mono on line 39 is a Mono<ExecutionResult>, meaning it is possible
    to perform additional operations depending on the command execution
    result.

--- src/main/java/net/gunivers/gunislave/command/CommandInitiator.java
+++ src/main/java/net/gunivers/gunislave/command/CommandInitiator.java
@@ -1,31 +1,42 @@
 package net.gunivers.gunislave.command;
 
-import discord4j.core.event.domain.message.MessageCreateEvent;
+import net.gunivers.gunislave.Main;
+
 import fr.theogiraudet.json_command_parser.CommandExecutor;
 import fr.theogiraudet.json_command_parser.CommandParser;
 import fr.theogiraudet.json_command_parser.Configuration;
-import net.gunivers.gunislave.Main;
 
-public class CommandInitiator {
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import reactor.core.publisher.Mono;
 
-    public static void initialize() {
-        // Definition of the command prefix
-        Configuration.setPrefix(",");
-        Configuration.setDebug(true);
+public class CommandInitiator
+{
+	public static void initialize()
+	{
+		// Definition of the command prefix
+		Configuration.setPrefix(",");
+		Configuration.setDebug(true);
 
-        // Definition of the package where commands classes are
-        CommandParser.parseCommands("net.gunivers.gunislave.command.commands");
+		// Definition of the package where commands classes are
+		CommandParser.parseCommands("net.gunivers.gunislave.command.commands");
 
-        // Listening to commands and then executing them
-        Main.getBotInstance().getBotClient().getEventDispatcher().on(MessageCreateEvent.class)
-                .filter(msg -> msg.getMessage()
-                        .getContent()
-                        .orElse("").startsWith(Configuration.getPrefix()))
-                .subscribe(msg -> CommandExecutor.execute(msg
-                        .getMessage()
-                        .getContent()
-                        .get()
-                        .substring(Configuration.getPrefix().length()), msg));
-    }
-
+		// Listening to commands and then executing them
+		Main.getBotInstance().getBotClient().getEventDispatcher()
+			.on(MessageCreateEvent.class)
+			.flatMap
+			(
+				event -> Mono
+					.justOrEmpty(event.getMessage().getContent())
+					.filter(msg -> msg.startsWith(Configuration.getPrefix()))
+					.flatMap
+					(
+						msg -> Mono.justOrEmpty(CommandExecutor.execute
+						(
+							msg.substring(Configuration.getPrefix().length()),
+							event
+						))
+					)
+			)
+			.subscribe();
+	}
 }
